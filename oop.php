63ecfe9d8e7ef40f1c1fe6f57d9d19c4b5ebe58a
morshedalam547 <157200324+morshedalam547@users.noreply.github.com>
2025-03-29 23:15:03 +0600

    add Person Info

--- oop.php
+++ oop.php
@@ -1,6 +1,27 @@
 <?php
 
-
 class person {
-	
+	public $name;
+	public $age;
+
+	public function getPersonInfo($name,$age){
+			$this->name=$name;
+			$this->age=$age;
+
+	}
+
+public function showPersonInfo(){
+	echo "name: $this->name <br>";
+	echo "age: $this->age <br>";
 }
+
+}
+
+
+$person1 = new person();
+$person1->getPersonInfo("Morshed", 25);
+$person1->showPersonInfo();
+echo "<br>";
+$person2 = new person();
+$person2->getPersonInfo("Sakib", 30);
+$person2->showPersonInfo();
