4399051e66bfd1c28157218da41d37c9277e2012
morshedalam547 <157200324+morshedalam547@users.noreply.github.com>
2025-03-30 22:57:48 +0600

    add

--- oop.php
+++ oop.php
@@ -31,3 +31,38 @@ $person3 = new person();
 $person3->getPersonInfo("Shakib", 35);
 $person3->showPersonInfo();
 
+
+
+
+class Fund{
+
+  public $fund;
+	 
+	public function __construct($initialFund){
+		$this-> fund = $initialFund;
+
+	}
+
+	public function addFund($amount){
+		$this->fund += $amount;
+	}
+
+	public function deductFund($amount){
+		$this->fund -= $amount;
+	}
+
+	public function getFund(){
+		return "Total fund is $this->fund";
+	}
+
+}
+
+
+
+$ourFund= new Fund(1000);
+$ourFund->addFund(500);
+$ourFund->deductFund(200);
+echo $ourFund->getFund();
+$ourFund->addFund(1000);
+echo $ourFund->getFund();
+echo "<br>";
